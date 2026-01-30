64bd2e5bb2eb0395aa0ad8734e2b923919731b8a
Skaydar92 <ftwbusch04@gmail.com>
2026-01-30 11:41:08 +0100

    added guest utils, flakes and ssh to vm host

--- hosts/nixos-server/configuration.nix
+++ hosts/nixos-server/configuration.nix
@@ -8,11 +8,15 @@
   imports = [
     ./disk-config.nix
   ];
+
   boot.loader.grub = {
     efiSupport = true;
     efiInstallAsRemovable = true;
   };
+
   services.openssh.enable = true;
+
+  services.qemuGuest.enable = true;
 
   networking = {
     firewall.enable = false;
@@ -38,6 +42,8 @@
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ7CNg80kTLQ8RdqVt/UT7No3qy4qaejHfWjx08qgSI4 felix@ThinkPad"
   ];
+
+  nix.settings.experimental-features = ["nix-command" "flakes"];
 
   system.stateVersion = "24.05";
 }
